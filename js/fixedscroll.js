2983a718287f56e28ada199970392ab19cd8a053
Milotrince <trinityjchung@gmail.com>
2020-01-23 21:42:23 -0800

    style phished page

--- js/fixedscroll.js
+++ js/fixedscroll.js
@@ -1,19 +1,5 @@
 $(document).ready(function() {
-    $(".typing-effect").each(function() {
-        var element = $(this)
-        var length = element.text().length + 1
-        var delay = parseInt(element.css("animation-delay").replace('s', ''))
-        element.css("visibility", "hidden")
-        element.css("animation",
-            `typing 2s steps(${length}, end) ${delay}s,
-            blink-caret 1s step-end 2 ${delay + 2}s`)
-        setTimeout(function() {
-            element.css("border-right-width", "0")
-        }, 4000 + delay * 1000)
-        setTimeout(function() {
-            element.css("visibility", "visible")
-        }, 10 + delay * 1000)
-    })
+    $("#content").css("margin-top", "516px")
 })
 
 $(window).scroll(function() {
